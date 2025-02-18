df8c7c4f295284f4f4ed065d322c1fa754db963f
Witallo Oliveira <witalloliveira@ipa.example.test>
2025-02-18 01:00:05 -0300

    sdx devtoolkit

--- install.sh
+++ install.sh
@@ -1,30 +1,112 @@
-#!/bin/bash
+#!/usr/bin/env sh
+set -eEuo pipefail
+source ./log.sh
 
-# Dummy Shell Script
+function init() {
 
-# Constants
-PROJECT_NAME="MyAwesomeProject"
-PROJECT_DIR="$HOME/$PROJECT_NAME"
+  OS=""
+  TMP_DIR=$(mktemp -d)
+  REPO="https://github.com/sennadx/sdx-developer-toolkit.git"
+  BRANCH="develop"
+  ANSIBLE_CFG="ansible.cfg"
+  ANSIBLE_PLAYBOOK="dummy.yml"
 
-# Functions
-create_project_dir() {
-  mkdir -p "$PROJECT_DIR"
-  echo "Created project directory: $PROJECT_DIR"
 }
 
-clone_repo() {
-  git clone https://github.com/your-org/your-repo.git "$PROJECT_DIR"
-  echo "Cloned repository to: $PROJECT_DIR"
+function check_os() {
+
+  [[ -f /etc/os-release ]] &&
+    OS=$(grep '^ID=' /etc/os-release | cut -d= -f2)
+
+  # mac os não possui /etc/os-release
+
 }
 
-create_hello_file() {
-  echo "Hello from my shell script!" > "$PROJECT_DIR/hello.txt"
-  echo "Created hello.txt file."
+function checkout() {
+
+  [[ -d "$3" ]] && cd "$3" && git clone "$1" -b "$2"
+
 }
 
-# Main Script
-create_project_dir
-clone_repo
-create_hello_file
+function tasks() {
 
-echo "Script completed successfully!"
+  #cp ~/workspace/sennadx/sdx-developer-toolkit/dummy.yml .
+  #cp ~/workspace/sennadx/sdx-developer-toolkit/ansible.cfg .
+  #cp ~/workspace/sennadx/sdx-developer-toolkit/roles/cloud/tasks/main.yml .
+
+  cp /sdx-developer-toolkit/dummy.yml .
+  cp /sdx-developer-toolkit/ansible.cfg .
+  cp /sdx-developer-toolkit/roles/cloud/tasks/main.yml .
+
+  ANSIBLE_CONFIG=${ANSIBLE_CFG} \
+    ansible-playbook $ANSIBLE_PLAYBOOK
+
+}
+
+function automation() {
+
+  [[ -d "$1" ]] && cd "$1" && tasks
+
+}
+
+function fedora_requirements() {
+
+  sudo dnf group install -y standard c-development \
+    development-libs development-tools --with-optional && \
+  sudo dnf install -y ansible ansible-collection-ansible-posix
+
+}
+
+function ubuntu_requirements() {
+
+  apt update && DEBIAN_FRONTEND=noninteractive apt install -y zip \
+    git build-essential ubuntu-standard ubuntu-minimal python3 ansible
+
+}
+
+
+function requirements() {
+
+  echo ""
+  echo "Installing Developer Toolkit requirements"
+  log info "This is a info message"
+  log warn "This is a warning message"
+  echo ""
+
+  case "$1" in
+
+  'fedora')
+    fedora_requirements
+    ;;
+
+  'ubuntu' | 'linuxmint')
+    ubuntu_requirements
+    ;;
+
+  esac
+
+}
+
+function cleanup() {
+
+  rm -rf $TMP_DIR
+
+}
+
+function main() {
+
+  init
+
+  check_os
+
+  requirements "${OS}"
+
+  #checkout "${REPO}" "${BRANCH}" "${TMP_DIR}"
+
+  automation "${TMP_DIR}"
+
+  cleanup
+
+}
+
+main
